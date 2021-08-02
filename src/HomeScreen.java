670dcaa410d76ae93c183246d453be91a4304c83
exagt <23314220+exagt@users.noreply.github.com>
2021-08-02 23:41:00 +0100

    finished login

--- src/HomeScreen.java
+++ src/HomeScreen.java
@@ -1,2 +1,49 @@
-public class HomeScreen {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class HomeScreen extends JFrame {
+    private JPanel panelMain;
+    private JLabel lblChoice;
+    private JButton btnCustomer;
+    private JButton btnStaff;
+    private JFrame frameMain;
+
+    public static void main(String[] args) {
+        HomeScreen homescreen = new HomeScreen();
+        homescreen.setVisible(true);
+    }
+
+    private HomeScreen() {
+        setContentPane(panelMain);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(350, 250));
+        pack();
+        frameMain = this;
+
+        btnCustomer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnStaff.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openLogin();
+            }
+        });
+    }
+
+    private void openLogin() {
+        Login admin = new Login(frameMain);
+        admin.setVisible(true);
+    }
+
+    private void openCustomer() {
+
+    }
 }
---
+++ src/Login.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame {
+    private JPanel panelMain;
+    private JTextField txtPass;
+    private JTextField txtUsername;
+    private JButton btnBack;
+    private JButton btnLogin;
+    private JLabel lblPass;
+    private JLabel lblUsername;
+
+    Login(JFrame homescreen){
+
+        Staff.loadAccounts();
+
+        setContentPane(panelMain);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(350, 250));
+        pack();
+
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                login(homescreen);
+            }
+        });
+
+        btnBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public void login(JFrame homescreen) {
+        if (Staff.checkAccount(txtUsername.getText(), txtPass.getText())) {
+//            Placeholder placeholder = new Placeholder(); //Needs changing
+//            placeholder.setVisible(true); //Needs changing
+            homescreen.dispose();
+            dispose();
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Staff login incorrect. Please try again.");
+        }
+    }
+}
+
