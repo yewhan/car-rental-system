c29dc9d74f60623fd06849729741fd08e722231e
exagt <23314220+exagt@users.noreply.github.com>
2021-08-08 17:30:24 +0100

    when closing receipt a new instance of homescreen is displayed in its place

--- src/Receipt.java
+++ src/Receipt.java
@@ -3,6 +3,8 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class Receipt extends JFrame {
     private JTextPane txtReceipt;
@@ -10,9 +12,10 @@ public class Receipt extends JFrame {
 
     public Receipt(String license, String carReg) {
         setContentPane(panelMain);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setPreferredSize(new Dimension(500, 500));
         pack();
+        Frame mainFrame = this;
 
         SimpleAttributeSet attributes = new SimpleAttributeSet();
         StyleConstants.setAlignment(attributes, StyleConstants.ALIGN_CENTER);
@@ -21,5 +24,44 @@ public class Receipt extends JFrame {
 
         System.out.println("Thread working on GUI: " + Thread.currentThread().getName());
         Threading thread = new Threading(txtReceipt, license, carReg);
+
+        mainFrame.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();
+                HomeScreen homescreen = new HomeScreen();
+                homescreen.setVisible(true);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
     }
 }
